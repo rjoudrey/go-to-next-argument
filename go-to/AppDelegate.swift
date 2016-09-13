9f19d3393ce046cf65e386779f33cc35da45414e
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2016-09-12 21:14:32 -0400

    Initial Commit

---
+++ go-to/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  go-to
+//
+//  Created by Ricky Joudrey on 9/12/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ go-to/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  go-to
+//
+//  Created by Ricky Joudrey on 9/12/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
